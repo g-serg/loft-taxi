fcc12ce50f882d5767319aa70b9a47257ad7a586
g-serg <mr-sergey2004@yandex.ru>
2022-03-22 18:32:11 +0300

    map support

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,7 +1,5 @@
-import React from "react";
+import React from 'react';
 
-function Profile() {
-  return <div>Profile</div>;
-}
-
-export default Profile;
+export const Profile = () => {
+  return <>Profile</>;
+};
